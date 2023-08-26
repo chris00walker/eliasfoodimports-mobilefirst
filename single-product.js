550985e163ffbc9c250758bf98d3ce5ab82477ab
chris00walker <chris00walker@gmail.com>
2023-08-25 23:01:19 -0400

    Change the script to mirror the shop details

--- single-product.js
+++ single-product.js
@@ -5,19 +5,40 @@ const productsData = [...featuredProductsData, ...newArrivalsData];
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DOMContentLoaded fired");  // Debug 1
 
-    function generateSingleProductHTML(product) {
-        const mainImageHTML = `<img src="${product.img}" width="100%" id="MainImg" alt="">`;
+    // function generateSingleProductHTML(product) {
+    //     const mainImageHTML = `<img src="${product.img}" width="100%" id="MainImg" alt="">`;
+    //     return `
+    // <div class="pro-container single-pro-image" role="list">
+    //     ${mainImageHTML}
+    // </div>
+    // <div class="single-pro-details">
+    //     <h6>${product.brand}</h6>
+    //     <h4>${product.name}</h4>
+    //     <h2>$${product.price}</h2>
+    // </div>
+    // `;
+    // }
+
+    function generateSingleProductHTML(product, index) {
         return `
-    <div class="pro-container single-pro-image" role="list">
-        ${mainImageHTML}
+<article class="product-card single-product-card">
+    <img src="${product.img}" alt="${product.alt}" class="product-image" data-index="${index}">
+    <div class="description">
+        <span class="product-brand">${product.brand}</span>
+        <h5 class="product-name">${product.name}</h5>
+        <div class="star">
+            ${'<i class="fas fa-star"></i>'.repeat(product.stars)}
+        </div>
+        <div class="product-info">
+            <span class="product-price">$${product.price}</span>
+            <button class="add-to-cart button">Add to Cart</button>
+            <span class="cart"><i class="fal fa-shopping-cart"></i></span>
+        </div>
     </div>
-    <div class="single-pro-details">
-        <h6>${product.brand}</h6>
-        <h4>${product.name}</h4>
-        <h2>$${product.price}</h2>
-    </div>
+</article>
     `;
     }
+
 
     function displaySingleProduct() {
         const index = getProductIdFromURL();
