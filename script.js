f46ed1df792010b065fbd7a76bb6e4ccd361f7f0
chris00walker <chris00walker@gmail.com>
2023-08-29 10:25:00 -0400

    Fix Add to Cart button to dynamically update cart icon

--- script.js
+++ script.js
@@ -1,47 +1,87 @@
 import { featuredProductsData, newArrivalsData } from './data.js';
+import { addToCart } from './cart.js';
 
-document.addEventListener("DOMContentLoaded", function () {
+try {
+    document.addEventListener("DOMContentLoaded", function () {
+        try {
+            // Function to generate HTML for a product
+            function createProductHTML(product, index) {
+                return `
+                    <article class="product-card">
+                        <a href="single-product.html?index=${index}">
+                            <img src="${product.img}" alt="${product.alt}" class="product-image" data-index="${index}">
+                        </a>
+                        <div class="description">
+                            <span class="product-brand">${product.brand}</span>
+                            <h5 class="product-name">${product.name}</h5>
+                            <div class="star">
+                                ${'<i class="fas fa-star"></i>'.repeat(product.stars)}
+                            </div>
+                            <div class="product-info">
+                                <span class="product-price">$${product.price}</span>
+                                <button class="add-to-cart button" data-product='${JSON.stringify(product)}'>Add to Cart</button>
+                                <span class="cart"><i class="fal fa-shopping-cart"></i></span>
+                            </div>
+                        </div>
+                    </article>
+                `;
+            }
 
-    // Function to generate HTML for a product
+            // Get product containers
+            const proContainer1 = document.querySelector('#featured-products .pro-container');
+            const proContainer2 = document.querySelector('#new-arrivals .pro-container');
 
-    function createProductHTML(product, index,) {
-        return `
-<article class="product-card">
-    <a href="single-product.html?index=${index}">
-        <img src="${product.img}" alt="${product.alt}" class="product-image" data-index="${index}">
-    </a>
-    <div class="description">
-        <span class="product-brand">${product.brand}</span>
-        <h5 class="product-name">${product.name}</h5>
-        <div class="star">
-            ${'<i class="fas fa-star"></i>'.repeat(product.stars)}
-        </div>
-        <div class="product-info">
-            <span class="product-price">$${product.price}</span>
-            <button class="add-to-cart button">Add to Cart</button>
-            <span class="cart"><i class="fal fa-shopping-cart"></i></span>
-        </div>
-    </div>
-</article>
-`;
-    }
+            if (!proContainer1 || !proContainer2) {
+                throw new Error("Product containers not found");
+            }
 
-    // Get product containers
-    const proContainer1 = document.querySelector('#featured-products .pro-container');
-    const proContainer2 = document.querySelector('#new-arrivals .pro-container');
+            // Generate and append product HTML for section 1
+            let featuredProductsHTML = '';
+            featuredProductsData.forEach((product, index) => {
+                featuredProductsHTML += createProductHTML(product, index);
+            });
+            proContainer1.innerHTML = featuredProductsHTML;
 
-    // Generate and append product HTML for section 1
-    let featuredProductsHTML = '';
-    featuredProductsData.forEach((product, index) => {
-        featuredProductsHTML += createProductHTML(product, index, 'featured');
+            // Generate and append product HTML for new-arrivals section
+            let newArrivalsHTML = '';
+            newArrivalsData.forEach((product, index) => {
+                newArrivalsHTML += createProductHTML(product, index);
+            });
+            proContainer2.innerHTML = newArrivalsHTML;
+
+            // Load cart size from local storage and update the icon
+            let cart;
+            try {
+                cart = JSON.parse(localStorage.getItem('cart')) || [];
+            } catch (e) {
+                console.error("Error parsing cart from local storage:", e);
+                cart = [];
+            }
+
+            const cartCount = cart.length;
+            document.querySelectorAll('.cart').forEach(cartIcon => {
+                cartIcon.innerHTML = `<i class="fal fa-shopping-cart"></i> ${cartCount}`;
+            });
+        } catch (e) {
+            console.error("An error occurred in DOMContentLoaded:", e);
+        }
     });
 
-    proContainer1.innerHTML = featuredProductsHTML;
-
-    // Generate and append product HTML for new-arrivals section
-    let newArrivalsHTML = '';
-    newArrivalsData.forEach((product, index) => {
-        newArrivalsHTML += createProductHTML(product, index, 'new');
+    document.body.addEventListener("click", function (event) {
+        try {
+            if (event.target.matches('.add-to-cart')) {
+                const productJSON = event.target.getAttribute('data-product');
+                const product = JSON.parse(productJSON);
+                addToCart(product);
+            }
+        } catch (e) {
+            console.error("An error occurred during click event:", e);
+        }
     });
-    proContainer2.innerHTML = newArrivalsHTML;
-})
+} catch (e) {
+    console.error("An error occurred in the script:", e);
+}
+
+
+
+
--- single-product.js
+++ single-product.js
@@ -1,65 +1,96 @@
 import { featuredProductsData, newArrivalsData } from './data.js';
+import { addToCart } from './cart.js';
 
 const productsData = [...featuredProductsData, ...newArrivalsData];
 
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOMContentLoaded fired");  // Debug 1
+    try {
+        console.log("DOMContentLoaded fired");  // Debug 1
 
-    // function generateSingleProductHTML(product) {
-    //     const mainImageHTML = `<img src="${product.img}" width="100%" id="MainImg" alt="">`;
-    //     return `
-    // <div class="pro-container single-pro-image" role="list">
-    //     ${mainImageHTML}
-    // </div>
-    // <div class="single-pro-details">
-    //     <h6>${product.brand}</h6>
-    //     <h4>${product.name}</h4>
-    //     <h2>$${product.price}</h2>
-    // </div>
-    // `;
-    // }
-
-    function generateSingleProductHTML(product, index) {
-        return `
-<article class="product-card single-product-card">
-    <img src="${product.img}" alt="${product.alt}" class="product-image" data-index="${index}">
-    <div class="description">
-        <span class="product-brand">${product.brand}</span>
-        <h5 class="product-name">${product.name}</h5>
-        <div class="star">
-            ${'<i class="fas fa-star"></i>'.repeat(product.stars)}
+        function generateSingleProductHTML(product) {
+            return `
+    <article class="product-card single-product-card">
+        <img src="${product.img}" alt="${product.alt}" class="product-image">
+        <div class="description">
+            <span class="product-brand">${product.brand}</span>
+            <h5 class="product-name">${product.name}</h5>
+            <div class="star">
+                ${'<i class="fas fa-star"></i>'.repeat(product.stars)}
+            </div>
+            <div class="product-info">
+                <span class="product-price">$${product.price}</span>
+                <button class="add-to-cart button" data-product='${JSON.stringify(product)}'>Add to Cart</button>
+                <span class="cart"><i class="fal fa-shopping-cart"></i></span>
+            </div>
         </div>
-        <div class="product-info">
-            <span class="product-price">$${product.price}</span>
-            <button class="add-to-cart button">Add to Cart</button>
-            <span class="cart"><i class="fal fa-shopping-cart"></i></span>
-        </div>
-    </div>
-</article>
-    `;
+    </article>
+        `;
+        }
+
+        function displaySingleProduct() {
+            const index = getProductIdFromURL();
+            console.log("Product Index from URL: ", index);  // Debug 2
+
+            if (index === null || index >= productsData.length) {
+                console.error("Invalid product index.");
+                return;
+            }
+
+            const product = getProductById(index);
+            console.log("Product Data: ", product);  // Debug 3
+
+            const productContainer = document.getElementById('product-container');
+
+            if (!productContainer) {
+                console.error("Product container not found.");
+                return;
+            }
+
+            const html = generateSingleProductHTML(product);
+            productContainer.innerHTML = html;
+        }
+
+        function getProductIdFromURL() {
+            const urlParams = new URLSearchParams(window.location.search);
+            const index = parseInt(urlParams.get('index'), 10);
+            return isNaN(index) ? null : index;
+        }
+
+        function getProductById(index) {
+            return productsData[index];
+        }
+
+        displaySingleProduct();
+
+        // Load cart size from local storage and update the icon
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        const cartCount = cart.length;
+        const cartIcons = document.querySelectorAll('.cart');
+
+        if (!cartIcons.length) {
+            console.error("Cart icons not found.");
+            return;
+        }
+
+        cartIcons.forEach(cartIcon => {
+            cartIcon.innerHTML = `<i class="fal fa-shopping-cart"></i> ${cartCount}`;
+        });
+    } catch (err) {
+        console.error("An error occurred: ", err);
     }
+});
 
-
-    function displaySingleProduct() {
-        const index = getProductIdFromURL();
-        console.log("Product Index from URL: ", index);  // Debug 2
-
-        const product = getProductById(index);
-        console.log("Product Data: ", product);  // Debug 3
-
-        const html = generateSingleProductHTML(product);
-        document.getElementById('product-container').innerHTML = html;
+document.body.addEventListener("click", function (event) {
+    if (event.target.matches('.add-to-cart')) {
+        try {
+            const productToAdd = JSON.parse(event.target.getAttribute('data-product'));
+            if (!productToAdd) {
+                console.error("Invalid product data.");
+                return;
+            }
+            addToCart(productToAdd);
+        } catch (err) {
+            console.error("An error occurred while adding to cart: ", err);
+        }
     }
-
-    function getProductIdFromURL() {
-        const urlParams = new URLSearchParams(window.location.search);
-        const index = parseInt(urlParams.get('index'), 10);
-        return index;
-    }
-
-    function getProductById(index) {
-        return productsData[index];
-    }
-
-    displaySingleProduct();
 });
